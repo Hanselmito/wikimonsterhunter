728878fb425d58015479630aaf4c9248313d486d
Hanselmito <151913571+Hanselmito@users.noreply.github.com>
2024-11-23 14:01:47 +0100

    porfavor no petes

--- src/main/java/com/github/Hanselmito/View/PrimaryController.java
+++ src/main/java/com/github/Hanselmito/View/PrimaryController.java
@@ -1,6 +1,8 @@
-package com.github.Hanselmito;
+package com.github.Hanselmito.View;
 
 import java.io.IOException;
+
+import com.github.Hanselmito.App;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
